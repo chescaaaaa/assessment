a10594ef1b781d725ea954bb87fdf905e92d4ea4
Chesca Moll <chescamoll31@gmail.com>
2021-12-26 01:44:04 +1300

    final edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,22 @@
-function addItem(){
-    
-    // get text field
-    const input = document.getElementById("inputElement");
-    const inputValue = input.value;
-    
-    // clear text field
-    input.value = "";
-    
-    console.log(inputValue);
-    
-    // create the list item
-    const item = document.createElement('li');
-    
-    // add the text for the list item
-    item.appendChild(document.createTextNode(inputValue));
-    
-    // get list
-    const list = document.getElementById("myList");
-    
-    // add it to the list
-    list.appendChild(item);
-    
+function addItem() {
+  // get text field
+  const input = document.getElementById("inputElement");
+  const inputValue = input.value;
+
+  // clear text field
+  input.value = "";
+
+  console.log(inputValue);
+
+  // create the list item
+  const item = document.createElement("li");
+
+  // add the text for the list item
+  item.appendChild(document.createTextNode(inputValue));
+
+  // get list
+  const list = document.getElementById("myList");
+
+  // add it to the list
+  list.appendChild(item);
 }
